0c3bd15c8981545dd7bb0e2911efe2a5d36c97db
EarnAnurat <anurat.wbm@gmail.com>
2023-01-11 00:46:59 +0000

    compush

--- application/routes.py
+++ application/routes.py
@@ -38,7 +38,8 @@ def videocatalog():
     videodata = vid.objects.all() # original
     # videodata = videos.objects.all() # cloud
     # return render_template("videocatalog.html", videodata=videodata, data2={"title2":title2, "thumbnail2":thumbnail2}, videocatalog=True)
-    return render_template("videocatalog.html", videodata=videodata, data2=jdata, videocatalog=True)
+    # return render_template("videocatalog.html", videodata=videodata, data2=jdata, videocatalog=True)
+    return render_template("videocatalog.html", data2=jdata, videocatalog=True)
 
 @app.route("/register") # 
 def register():
